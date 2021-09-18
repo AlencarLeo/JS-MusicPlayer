49759d83e2efc63392260c0ac534384444fc6eea
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2021-09-17 22:59:07 -0300

    more updates

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const title = document.querySelector('.title');
+const audio = document.querySelector('audio');
+
+const previous = document.getElementById('previous');
+const playPause = document.getElementById('playPause');
+const next = document.getElementById('next');
+
+const songList = [
+    {
+        name: "Kasino - Can't get over",
+        path: "../img/cantGetOver.mp3"
+    },
+    {
+        name: "Seu Madruga - Noite serena (funk version), feat. Girafales",
+        path: "../img/funkNoiteSerena.mp3"
+    },
+    {
+        name: "Faustão - Ovo cozido",
+        path: "../img/ovoFausto.mp3"
+    },
+    {
+        name: "Bill Gates - Teclado lindinho",
+        path: "../img/tecladoLindinho.mp3"
+    },
+];
+
