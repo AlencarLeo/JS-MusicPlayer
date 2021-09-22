aa29bce1f3a4675f31ab234e8308e3035826dfb9
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2021-09-22 00:20:40 -0300

    more updates

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const previous = document.getElementById('previous');
 const playPause = document.getElementById('playPause');
 const next = document.getElementById('next');
 
+// SONG LIST
 const songList = [
     {
         name: "Kasino - Can't get over",
@@ -24,3 +25,61 @@ const songList = [
     },
 ];
 
+let songPlaying = false;
+
+// PLAY SONG
+function playSong(){
+    songPlaying = true;
+    audio.play();
+    playPause.classList.add('play');
+    playPause.innerHTML = '<i class="fas fa-pause"></i>'
+}
+
+// PAUSE SONG
+function pauseSong(){
+    songPlaying = false;
+    audio.pause();
+    playPause.classList.remove('play');
+    playPause.innerHTML = '<i class="fas fa-play"></i>'
+}
+
+
+// PLAY OR PAUSE SONG ON CLICK
+playPause.addEventListener('click', () => songPlaying ? pauseSong() : playSong() );
+
+// LOAD SONG
+function loadSong(songList){
+    title.innerText = songList.name;
+    audio.src = songList.path;
+}
+
+// CURRENT SONG 
+let i = 0;
+
+// FIRST SONG ON PLAY
+loadSong(songList[i]);
+
+// PREVIOUS SONG
+function previousSong(){
+    i--;
+    if( i < 0){
+        i = songList.length - 1;
+    }
+    loadSong(songList[i]);
+    playSong();
+}
+
+previous.addEventListener('click', previousSong);
+
+// NEXT SONG
+
+function nextSong(){
+    i++;
+    if( i > songList.length - 1){
+        i = 0;
+    }
+    loadSong(songList[i]);
+    playSong();
+}
+
+next.addEventListener('click', nextSong);
